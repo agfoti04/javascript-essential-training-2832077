516bfc7e81f5e5a592492ae446e5be48f3052ace
antfoti <a.g.foti1@gmail.com>
2025-03-29 13:05:34 -0500

    Practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,7 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const school = {
+    student_count : 70000000
+};
